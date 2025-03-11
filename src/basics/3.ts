0dffda591d69171c2a96829ac75202820fb175de
Anna Bieliavtseva <belyavtseva1911@gmail.com>
2025-03-11 09:09:18 +0100

    fixed union type

--- src/basics/3.ts
+++ src/basics/3.ts
@@ -1,5 +1,8 @@
-type unionTypeVar = 'enable' | 'disable';
-let unionVar: unionTypeVar = 'enable';
-let union1Var: unionTypeVar = 'disable';
+type unionTypeVar = string | number;
+type literalTypeVar = 'enable' | 'disable';
+let unionVar: unionTypeVar = 2;
+let union1Var: unionTypeVar = 'hello';
+let literalVar: literalTypeVar = 'disable';
+let literal1Var : literalTypeVar = 'enable'
 
-console.log(unionVar, union1Var);
+console.log(unionVar, union1Var, literal1Var, literalVar);
